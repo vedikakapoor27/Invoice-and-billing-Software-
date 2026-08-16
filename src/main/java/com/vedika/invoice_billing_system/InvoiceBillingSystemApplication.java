42424f74592cfd5d9493819da70109d4cace13c1
vedikakapoor27 <vedikakapoor2722004@gmail.com>
2026-08-16 20:09:51 +0530

    update changes

--- src/main/java/com/vedika/invoice_billing_system/InvoiceBillingSystemApplication.java
+++ src/main/java/com/vedika/invoice_billing_system/InvoiceBillingSystemApplication.java
@@ -1,4 +1,4 @@
-package com.vedika.invoice_billing_system;
+ package com.vedika.invoice_billing_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
